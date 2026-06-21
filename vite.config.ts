4044dd46bb4fffb4b66f1798b0ea044faea5a73b
vi-wolhwa <cerezo00@naver.com>
2026-06-21 16:39:59 +0900

    chore: vite-plugin-svgr 설치

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import path from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
