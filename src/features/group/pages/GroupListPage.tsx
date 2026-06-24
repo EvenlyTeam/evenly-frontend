5a3b5739a7aa0f39361a26aade1196a3980ed2da
vi-wolhwa <cerezo00@naver.com>
2026-06-24 14:53:57 +0900

    feat(group-list): GroupList Grid 구현

--- src/features/group/pages/GroupListPage.tsx
+++ src/features/group/pages/GroupListPage.tsx
@@ -3,6 +3,15 @@ import { Navbar } from '@/components';
 import { GroupStyleWrapper } from '../components';
 
 export function GroupListPage() {
+  const items = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+  ];
   return (
     <GroupStyleWrapper>
       <GroupStyleWrapper.Navbar>
@@ -12,7 +21,17 @@ export function GroupListPage() {
         </Navbar>
       </GroupStyleWrapper.Navbar>
 
-      <GroupStyleWrapper.Body>content</GroupStyleWrapper.Body>
+      <GroupStyleWrapper.Body>
+        <div className="text-heading-md text-foreground lg:text-heading-lg">
+          내 모임
+        </div>
+
+        <div className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {items.map((item) => (
+            <div key={item.id}>{item.id}</div>
+          ))}
+        </div>
+      </GroupStyleWrapper.Body>
     </GroupStyleWrapper>
   );
 }
