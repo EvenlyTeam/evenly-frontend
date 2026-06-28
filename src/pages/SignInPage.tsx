528a2c46b43aa591307e7490c78e35d6a454de25
vi-wolhwa <cerezo00@naver.com>
2026-06-28 18:23:46 +0900

    fix(code-formatting): 로그인/가입 페이지 Device 잔재 제거

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,18 +1,20 @@
-import { Desktop } from '@/components';
 import {
   AuthBrandPanel,
   AuthSignInFormPanel,
   AuthStyleWrapper,
 } from '@/features/auth/components';
+import { useIsDesktop } from '@/hooks/useIsDesktop';
 
 export function SignInPage() {
+  const isDesktop = useIsDesktop();
+
   return (
     <AuthStyleWrapper>
-      <Desktop>
+      {isDesktop && (
         <AuthStyleWrapper.BrandPanel>
           <AuthBrandPanel />
         </AuthStyleWrapper.BrandPanel>
-      </Desktop>
+      )}
 
       <AuthStyleWrapper.FormPanel>
         <AuthSignInFormPanel />
